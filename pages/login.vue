1e04ccd6be5d65b823efc656deac60e9780bc032
pla2544 <ita5909@ccollege.ac.th>
2020-07-10 14:01:18 +0700

    s

---
+++ pages/login.vue
@@ -0,0 +1,24 @@
+<template>
+ <v-app>
+   <v-card>
+     <v-card-title>
+       <h1>Login</h1>
+
+
+     </v-card-title>
+     <v-card-text>
+       <v-form>
+         <v-text-field label="Username" prepend-icon="account_circle" />
+         <v-text-field type="password" label="Password" prepend-icon="lock" append-icon="visibility_off"/>
+       </v-form>
+     </v-card-text>
+     <v-card-actions>
+         <nuxt-link to="/#/">
+    <v-btn class="ma-2" tile color="green darken-4" dark>REGISTER</v-btn>
+    </nuxt-link>
+         <v-spacer></v-spacer>
+         <v-btn color="info">login </v-btn>
+     </v-card-actions>
+   </v-card>
+ </v-app>
+</template>
