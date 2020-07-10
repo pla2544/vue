95347336bff2bd3909799a1d7efe9ff5fd06dfd7
pla2544 <ita5909@ccollege.ac.th>
2020-07-10 14:47:45 +0700

    start

--- pages/login.vue
+++ pages/login.vue
@@ -1,15 +1,23 @@
 <template>
  <v-app>
-   <v-card>
+     <center>
+         <br>
+   <v-card width="400" color="#D2691E">
+       
      <v-card-title>
        <h1>Login</h1>
-
-
      </v-card-title>
+
      <v-card-text>
        <v-form>
          <v-text-field label="Username" prepend-icon="account_circle" />
-         <v-text-field type="password" label="Password" prepend-icon="lock" append-icon="visibility_off"/>
+         <v-text-field 
+            :type="showPassword ? 'text' : 'password'" 
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
        </v-form>
      </v-card-text>
      <v-card-actions>
@@ -20,5 +28,17 @@
          <v-btn color="info">login </v-btn>
      </v-card-actions>
    </v-card>
+   </center>
  </v-app>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
